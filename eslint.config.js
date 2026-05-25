f09c30b31af1d7ff563868d7215a51aa88a1baa8
Matheus Sartori <ms.sartori@outlook.com.br>
2026-05-25 20:07:04 -0300

    ci: add action

--- eslint.config.js
+++ eslint.config.js
@@ -7,15 +7,18 @@ import pluginPromise from 'eslint-plugin-promise'
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  pluginN.configs['flat/recommended-module'],
+  pluginPromise.configs['flat/recommended'],
   {
-    plugins: {
-      n: pluginN,
-      promise: pluginPromise,
-    },
     rules: {
-      'promise/param-names': 'error',
       'n/no-process-exit': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'off',
+    },
+  },
+  {
+    files: ['eslint.config.js'],
+    rules: {
+      'n/no-unpublished-import': 'off',
     },
   },
   {
